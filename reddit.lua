1a7d6ac932bbabe58530d272c218f52687b201bb
William Jagels <will.jagels@gmail.com>
2014-11-16 02:40:06 -0500

    add more functions

--- reddit.lua
+++ reddit.lua
@@ -5,11 +5,23 @@ function onPoseEdge(pose, edge)
 	if isChrome then
 		myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
 		if pose == "fist" and edge == "on" and myo.getPitch() < -0.6 then
-			myo.keyboard("pageup", "press")
-			myo.debug(myo.getPitch())
+			myo.keyboard("k", "press")
 		elseif pose == "fist" and edge == "on" and myo.getPitch() > 0.6 then
-			myo.keyboard("pagedown", "press")
-			myo.debug(myo.getPitch())
+			myo.keyboard("j", "press")
+		elseif pose == "waveIn" and edge == "on" then
+			if myo.getPitch() > 0.6 then
+				myo.keyboard("j", "press")
+			elseif myo.getPitch() < -0.6 then
+				myo.keyboard("k", "press")
+			else
+				myo.keyboard("x", "press")
+			end
+		elseif pose == "fingersSpread" and edge == "on" then
+			if myo.getPitch() < -0.6 then
+				myo.keyboard("a", "press")
+			elseif myo.getPitch() > 0.6 then
+				myo.keyboard("z", "press")
+			end
 		end
 	end
 end
@@ -18,7 +30,6 @@ function onPeriodic()
 end
 
 function onForegroundWindowChange(app, title)
-    myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
     local x = 0
 	if(title:match("Google Chrome")) then
 		myo.vibrate("short") x = x + 1  
